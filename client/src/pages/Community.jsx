6ea5998355b6eb8818c78aefbed09db53828f9f5
Abhishek Upadhyay <au8778166@gmail.com>
2025-10-20 03:00:02 +0530

    Design Community page, RemoveBackground Page, Remove Object Page, Generate Images pages, Resume reveiw and with it I have complete my frontend part

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useUser } from '@clerk/clerk-react'
+import React, { useEffect, useState } from 'react'
+import { dummyPublishedCreationData } from '../assets/assets'
+import { Heart } from 'lucide-react'
 
 const Community = () => {
+  const [creations, setCreations] = useState([])
+  const { user } = useUser()
+
+  const fetchCreations = async () => {
+    setCreations(dummyPublishedCreationData)
+  }
+
+  useEffect(() => {
+    if (user) {
+      fetchCreations()
+    }
+  }, [user])
+
   return (
-    <div>
-        <h1>Community</h1>
+    <div className="flex-1 h-full flex flex-col gap-4 p-6">
+      <h1 className="text-xl font-semibold text-slate-700">Creations</h1>
+
+      {/* Gallery container */}
+      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll p-4 flex flex-wrap gap-4">
+        {creations.map((creation, index) => (
+          <div
+            key={index}
+            className="relative group w-full sm:w-[48%] lg:w-[32%] rounded-lg overflow-hidden shadow-sm hover:shadow-md transition"
+          >
+            {/* Image */}
+            <div className="aspect-[4/3] w-full overflow-hidden rounded-lg relative">
+              <img
+                src={creation.content}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+
+              {/* Prompt and like (visible only on hover) */}
+              <div
+                className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 
+                transition-all duration-300 flex justify-between items-center px-3 py-2"
+              >
+                <p className="text-sm text-white bg-black/60 px-2 py-1 rounded-md truncate w-3/4">
+                  {creation.prompt}
+                </p>
+
+                <div className="flex gap-1 items-center text-white">
+                  <p>{creation.likes?.length || 0}</p>
+                  <Heart
+                    className={`min-w-5 h-5 hover:scale-110 cursor-pointer transition 
+                      ${
+                        creation.likes?.includes(user?.id)
+                          ? 'fill-red-500 text-red-600'
+                          : 'text-white'
+                      }`}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
